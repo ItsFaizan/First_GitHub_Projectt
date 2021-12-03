69ef3335a37c32222be6f38c5b03c68448d54f13
Faizan Javed <faizanjavid71@gmail.com>
2021-12-03 10:08:27 +0500

    first project

---
+++ src/First_File.java
@@ -0,0 +1,12 @@
+
+public class First_File {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Faizan";
+		String rollno = "20i-1832";
+		System.out.println(",mdhfsdfksdfsdbfjsdbfgbsdfgbsdklgbsdjo " + name);
+		System.out.println("RollNo: " + rollno);
+	}
+
+}
