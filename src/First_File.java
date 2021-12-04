e45b00ef5c0720200dafec997db93c6a7ff79414
Faizan Javed <faizanjavid71@gmail.com>
2021-12-04 18:43:28 +0500

    commit

--- src/First_File.java
+++ src/First_File.java
@@ -3,8 +3,8 @@ public class First_File {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name = "Momin";
-		String rollno = "18i-0574";
+		String name = "Faizan";
+		String rollno = "20i-1832";
 		System.out.println("Name" + name);
 		System.out.println("RollNo: " + rollno);
 	}
